abd5408c68b53f465842be7905562c3e5148d5a1
minhyup <minhyupp@gamil.com>
2020-04-08 14:51:00 +0900

    ddd

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React by minhyup
         </a>
       </header>
     </div>
